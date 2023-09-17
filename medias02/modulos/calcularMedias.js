6dc59bbd5dbe78e625ef02e19c266f1b7fbb0e69
caaaampos444 <gustavock18@hotmail.com>
2023-09-17 03:40:18 -0300

    alteraçoes no exercicio 2

--- medias02/modulos/calcularMedias.js
+++ medias02/modulos/calcularMedias.js
@@ -4,23 +4,13 @@
 * Autor: Gustavo Campos
 * Versão: 1.0
 *****************************************************/
-var readline=require('readline')
-var entradaDados=readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
-})
 const calcularMedias=function(valorN1,valorN2,valorN3,valorN4){
-    let nomeAluno
-    let nomeProf
-    let sexoAluno
-    let sexoProf
-    let curso
     let nota1=valorN1
     let nota2=valorN2
     let nota3=valorN3
     let nota4=valorN4
     let status
-    if(nota1==''||nota2==''||nota3==''||nota4==''||nomeAluno==''||nomeProf==''||sexoAluno==''||sexoProf==''||curso=='')
+    if(nota1==''||nota2==''||nota3==''||nota4=='')
         console.log('ERRO: É obrigatório a digitação de todos os valores.')
     else if(isNaN(nota1)||isNaN(nota2)|| isNaN(nota3)||isNaN (nota4))
         console.log('ERRO: É obrigatório somente a digitação de valores numéricos.')
@@ -31,15 +21,13 @@ const calcularMedias=function(valorN1,valorN2,valorN3,valorN4){
         console.log('MÉDIA: '+`${media.toFixed(2)}`)
     }
     if(media>70)
-        status='APROVADO'
+        return status='APROVADO'
     else if(media<50){
-        status='REPROVADO'
+        return status='REPROVADO'
     }
-    return status
-    /*else if(media>=50&&media<=69){
-         entradaDados.question('Aluno em exame. Digite a quinta nota: ',function(nota5Usuario){
-            nota5=nota5Usuario
-         })
-    }*/
+}
+const calcularMediasExame=function(valorMedia){
+    
 }
 module.exports={calcularMedias}
+//module.exports={calcularMediasExame}
